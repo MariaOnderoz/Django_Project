bf8809320bf91f1eda4c0cb88c883ca559e69610
mariaonderoz <mskorneeva.87@gmail.com>
2024-05-08 15:51:38 +0700

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from catalog.views import home, contacts, product_detail
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, contacts
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', contacts, name='contacts'),
-    path('catalog/<int:pk>/', product_detail, name='product_detail')
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/create/', ProductCreateView.as_view(), name='product_create'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from catalog.models import Product
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {
-        "products": products
-    }
-    return render(request, 'catalog/home.html', context)
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
 
 
 def contacts(request):
@@ -21,9 +26,37 @@ def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'catalog/product_detail.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ("name", "description", "image", "category", "price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("name", "description", "image", "category", "price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
+
+
+
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"product": product}
+#     return render(request, 'catalog/product_detail.html', context)
+
+# def home(request):
+#     products = Product.objects.all()
+#     context = {
+#         "products": products
+#     }
+#     return render(request, 'catalog/product_list.html', context)
+
+
+
 
 
